d80a6664c9e6cab6f029521aab34f9054eabf94d
Jean Patimeteeporn <jpatimet@gmail.com>
2024-01-24 03:14:05 -0600

    added employeeprofile functionality

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -7,6 +7,7 @@ import { CustomerDetails } from "../components/customers/CustomerDetails";
 import { EmployeeList } from "../components/employees/EmployeeList";
 import { EmployeeDetails } from "../components/employees/EmployeeDetails";
 import { useEffect, useState } from "react";
+import { EmployeeForm } from "../forms/EmployeeForm";
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({});
 
@@ -45,6 +46,10 @@ export const ApplicationViews = () => {
             <Route index element={<EmployeeList />} />
             <Route path=":employeeId" element={<EmployeeDetails />} />{" "}
           </Route>
+          <Route
+            path="profile"
+            element={<EmployeeForm currentUser={currentUser} />}
+          />
         </Route>
       </Routes>
     </>
